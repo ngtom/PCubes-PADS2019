cc84cc3627b2c87eaba2aee71a0aaf4e3a8596d3
Lukas Schade <lukas.schade@rwth-aachen.de>
2019-05-14 21:20:24 +0200

    upload page

--- process_cubes/dimension_editor/views.py
+++ process_cubes/dimension_editor/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from import_xes.models import EventLog
+from import_xes.models import EventLog, Dimension
 
 # Create your views here.
 
 def dimension_edit(request, pk):
     log = EventLog.objects.get(pk=pk)
     
+    dimensions = Dimension.objects.get(log=log)
