c6642bbdbf689f44bb0d166e5b2eae6d41cbc8ad
Thomas <slash_tom@yahoo.com>
2019-05-17 13:39:23 +0200

    pcv html added

--- process_cubes/PCV/views.py
+++ process_cubes/PCV/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from import_xes.models import EventLog, Dimension, Attribute
+
 # Create your views here.
 
 
 def createPCV(request, pk):
-    pass
+    log = EventLog.objects.get(pk=pk)
+    dimensions = Dimension.objects.filter(log=log)
+
+    #return redirect(dimension_edit, pk=log_id)
+
+    return render(request, 'pcv/pcv.html',
+                  {
+                      'log': log,
+                      'dimensions': dimensions,
+                      #'attributes': attr_names
+                  })
