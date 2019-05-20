b6e8fd18acdc38b07773eab8079a45a394637c84
Thomas <slash_tom@yahoo.com>
2019-05-20 23:56:08 +0200

    add attributes

--- process_cubes/dimension_editor/views.py
+++ process_cubes/dimension_editor/views.py
@@ -26,18 +26,14 @@ def dimension_edit(request, log_id, cube_id):
 
     attributes = Attribute.objects.filter(log=log)
 
-    used_attributes = [
-        attr for dim in dimensions for attr in dim.attributes.all()]
-    free_attributes = [
-        attr for attr in attributes if attr not in used_attributes]
+    used_attributes = [attr for dim in dimensions for attr in dim.attributes.all()]
+    free_attributes = [attr for attr in attributes if attr not in used_attributes]
 
     for dim in dimensions:
         if(len(dim.attributes.all()) > 0):
-            dim.num_elements = reduce(
-                mul, [len(attr.values) for attr in dim.attributes.all()], 1)
+            dim.num_elements = reduce(mul, [len(attr.values) for attr in dim.attributes.all()], 1)
 
-    cells = reduce(
-        mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
+    cells = reduce(mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
 
     return render(request, 'dimension_editor/main.html',
                   {
@@ -69,8 +65,7 @@ def add_attribute(request, log_id, cube_id):
     if(len(dimension.attributes.all()) == 0):
         num_elements = 0
     else:
-        num_elements = reduce(mul, [len(attr.values)
-                                    for attr in dimension.attributes.all()], 1)
+        num_elements = reduce(mul, [len(attr.values) for attr in dimension.attributes.all()], 1)
 
     dimension.num_elements = num_elements
     dimension.save()
@@ -79,11 +74,9 @@ def add_attribute(request, log_id, cube_id):
     dimensions = Dimension.objects.filter(cube=cube)
     for dim in dimensions:
         if(len(dim.attributes.all()) > 0):
-            dim.num_elements = reduce(
-                mul, [len(attr.values) for attr in dim.attributes.all()], 1)
+            dim.num_elements = reduce(mul, [len(attr.values) for attr in dim.attributes.all()], 1)
 
-    cells = reduce(
-        mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
+    cells = reduce(mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
 
     data = {'dim': dimension, 'attribute': attribute}
     html = render_to_string('dimension_editor/attribute.html', data, request)
@@ -106,23 +99,19 @@ def rem_attribute(request, log_id, cube_id):
     dimensions = Dimension.objects.filter(cube=cube)
     for d in dimensions:
         if(len(dim.attributes.all()) > 0):
-            d.num_elements = reduce(
-                mul, [len(attr.values) for attr in d.attributes.all()], 1)
+            d.num_elements = reduce(mul, [len(attr.values) for attr in d.attributes.all()], 1)
 
-    cells = reduce(
-        mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
+    cells = reduce(mul, [dim.num_elements for dim in dimensions if dim.num_elements != 0], 1)
 
     if(len(dim.attributes.all()) == 0):
         num_elements = 0
     else:
-        num_elements = reduce(mul, [len(attr.values)
-                                    for attr in dim.attributes.all()], 1)
+        num_elements = reduce(mul, [len(attr.values) for attr in dim.attributes.all()], 1)
 
     print(dim.pk)
 
     data = {'attribute': attribute}
-    html = render_to_string(
-        'dimension_editor/dropdown_button.html', data, request)
+    html = render_to_string('dimension_editor/dropdown_button.html', data, request)
     ret = {"html": html, 'num_elements': num_elements, 'cells': cells}
 
     return JsonResponse(ret)
@@ -144,10 +133,8 @@ def add_dimension(request, log_id, cube_id):
     new_dim = Dimension.objects.create(cube=cube, name='Dimension')
 
     attributes = Attribute.objects.filter(log=log)
-    used_attributes = [
-        attr for dim in dimensions for attr in dim.attributes.all()]
-    free_attributes = [
-        attr for attr in attributes if attr not in used_attributes]
+    used_attributes = [attr for dim in dimensions for attr in dim.attributes.all()]
+    free_attributes = [attr for attr in attributes if attr not in used_attributes]
 
     data = {'dim': new_dim, 'free_attributes': free_attributes}
     return render(request, 'dimension_editor/dimension.html', data)
