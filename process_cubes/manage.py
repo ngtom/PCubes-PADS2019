2d51bbeaf1573f2351b6004b9a91f7c07f31c8a2
Muhammad Umer <umer@dropslab.com>
2019-05-23 18:04:51 +0200

    Testing Configuration of GIT: Muhammad UMER

--- process_cubes/manage.py
+++ process_cubes/manage.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "process_cubes.settings")
     try:
         from django.core.management import execute_from_command_line
+        print('Tesing 123')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
