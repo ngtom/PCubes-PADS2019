dc847d24752d01e70f531e3039599bd418768de1
Lukas Schade <lukas.schade@rwth-aachen.de>
2019-06-08 20:09:16 +0200

    slice-dice

--- process_cubes/slice_dice/models.py
+++ process_cubes/slice_dice/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from djongo import models
+from import_xes.models import EventLog, ProcessCube, Dimension, Attribute
 
 # Create your models here.
+
+
+class AttributeRestriction(models.Model):
+    attribute = models.ForeignKey(to=Attribute, on_delete=models.CASCADE)
+    value = models.CharField()
+
+    class Meta:
+        abstract = True
+
+class DimensionRestriction(models.Model):
+    values = models.ArrayModelField(model_container=AttributeRestriction)
+
+    class Meta:
+        abstract = True
+
+class Slice(models.Model):
+    dimension = models.ForeignKey(to=Dimension, on_delete=models.CASCADE)
+    values = models.ArrayModelField(model_container=DimensionRestriction)
--- process_cubes/slice_dice/urls.py
+++ process_cubes/slice_dice/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:cube_id>/dim/<int:dim_id>/slice', views.slice, name='dimension-edit'),
+    path('<int:cube_id>/slice/<int:dim_id>/', views.slice_operation, name='slice'),
+    path('<int:cube_id>/slice/<int:dim_id>/save_slice', views.save_slice, name='save-slice'),
+    path('<int:cube_id>/dice/<int:dim_id>/', views.dice_operation, name='dice'),
 ]
