a238298cfed62c74ff288e632162cfd1c8ab59c6
Lukas Schade <lukas.schade@rwth-aachen.de>
2019-06-18 19:01:07 +0200

    Revert "Merge remote-tracking branch 'origin/hierarchy' into hierarchy"
    This reverts commit c80e49237f55652508122302b250a98e3eabf83c.

--- process_cubes/slice_dice/urls.py
+++ process_cubes/slice_dice/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:cube_id>/slice/<int:dim_id>/', views.slice_operation, name='slice'),
-    path('<int:cube_id>/slice/<int:dim_id>/save_slice', views.save_slice, name='save-slice'),
-    path('<int:cube_id>/dice/<int:dim_id>/save_dice', views.save_dice, name='save-dice'),
-    path('<int:cube_id>/dice/<int:dim_id>/', views.dice_operation, name='dice'),
+    path('<int:cube_id>/dim/<int:dim_id>/slice', views.slice, name='dimension-edit'),
 ]
--- process_cubes/slice_dice/views.py
+++ process_cubes/slice_dice/views.py
@@ -1,130 +1,29 @@
-from django.shortcuts import render, redirect
-import json
-import numbers
-from import_xes.models import EventLog, Attribute, ProcessCube, Dimension
-from django.http import HttpResponse
-from .models import AttributeRestriction, Slice, Dice, DimensionRestriction
-from .forms import DateFilter, NumberFilter, StringFilter
-import datetime
-from cells_list.views import get_dim_values
-from PCV.views import createPCV
+from django.shortcuts import render
+from import_xes.models import EventLog, Dimension, Attribute, ProcessCube
+
 # Create your views here.
 
-def make_name(a):
-    name = a.name
-    if(a.parent != 'event'):
-        name = a.parent + ':' + name
 
-    return name
-
-def operation(request, log_id, cube_id, dim_id, page):
+def slice(request, log_id, cube_id, dim_id):
     logs = EventLog.objects.all()
-
     log = EventLog.objects.get(pk=log_id)
-    cube = ProcessCube.objects.get(pk=cube_id)
     cubes = ProcessCube.objects.filter(log=log)
-    attributes = Attribute.objects.filter(log=log)
-    dimension = Dimension.objects.get(pk=dim_id)
+    cube = ProcessCube.objects.get(pk=cube_id)
+    dimension = Dimension.objects.filter(pk=dim_id)
 
-    filters = []
+    print(dim_id)
+    print(dimension.name)
 
-    for attr in dimension.attributes.all():
-        if(isinstance(attr.values[0], (int, float))):
-            filter_form = NumberFilter()
-        elif(isinstance(attr.values[0], datetime.datetime)):
-            filter_form = DateFilter()
-        else:
-            filter_form = StringFilter()
-                
-        filter_form.attribute = attr
-        filters.append(filter_form)
-
-    attr_names = [make_name(a) for a in dimension.attributes.all()]
-
-    dim_values = get_dim_values(dimension)
-    dim_values = [[str(v) for v in values] for values in dim_values]
-
-    dim_values = [[''] + row for row in dim_values]
-
-    return render(request, page, {
-        'logs': logs,
+    return render(request, "slice.html", {
         'log': log,
         'cube': cube,
+        'logs': logs,
         'cubes': cubes,
-        'attributes': attributes,
-        'dimension': dimension,
-        'attr_names': attr_names,
-        'filters': filters,
-        'dim_values': dim_values})
+        'dim': dimension
+    })
 
-def slice_operation(request, log_id, cube_id, dim_id):
-    return operation(request, log_id, cube_id, dim_id, 'slice_dice/slice.html')
-
-def dice_operation(request, log_id, cube_id, dim_id):
-    return operation(request, log_id, cube_id, dim_id, 'slice_dice/dice.html')
-
-
-def save_dice(request, log_id, cube_id, dim_id):
-    dimension = Dimension.objects.get(pk=dim_id)
-    values = request.POST.getlist("values[]")
-
-    values_dict = []
-    attributes = {make_name(attr): attr for attr in dimension.attributes.all()}
-
-    for vs_json in values:
-        vs = json.loads(vs_json)
-        
-        restr = {}
-        for i, attr in enumerate(dimension.attributes.all()):
-            restr[make_name(attr)] =  vs[i]
-
-        values_dict.append(restr)
-        
     
-    dim_values = []
-    for value_restr in values_dict:
-        attr_restrictions = []
-        for attr in value_restr:
-            attribute = attributes[attr]
-            value = value_restr[attr]
-            restr = AttributeRestriction(attribute=attribute, value=value)
-            attr_restrictions.append(restr)
 
-        dim_restr = DimensionRestriction(values=attr_restrictions)
-        dim_values.append(dim_restr)
-
-    dice_obj = Dice(dimension=dimension, values=dim_values)
-    dice_obj.save()
-
-    return redirect(createPCV, log_id, cube_id)
-
-
-def save_slice(request, log_id, cube_id, dim_id):
-    dimension = Dimension.objects.get(pk=dim_id)
-    values = request.POST.get("values")
-
-    attributes = {make_name(attr): attr for attr in dimension.attributes.all()}
-
-
-    vs = json.loads(values)
-    value_restr = {}
-    for i, attr in enumerate(dimension.attributes.all()):
-        value_restr[make_name(attr)] =  vs[i]
-
-        
+def dice_operation(request, pk):
+    log = EventLog.objects.get(pk=pk)
     
-    attr_restrictions = []
-    for attr in value_restr:
-        attribute = attributes[attr]
-        value = value_restr[attr]
-        restr = AttributeRestriction(attribute=attribute, value=value)
-        attr_restrictions.append(restr)
-
-    dim_restr = DimensionRestriction(values=attr_restrictions)
-
-    slice_obj = Slice(dimension=dimension, value=dim_restr)
-    slice_obj.save()
-
-    return redirect(createPCV, log_id, cube_id)
-
-
