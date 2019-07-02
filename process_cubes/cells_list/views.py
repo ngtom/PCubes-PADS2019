2a8e0586962ae72abd3da6e4ead9ebf132217d75
Lukas <lukas.schade@rwth-aachen.de>
2019-07-02 09:36:07 +0200

    do not sort here

--- process_cubes/cells_list/views.py
+++ process_cubes/cells_list/views.py
@@ -79,7 +79,7 @@ def get_restricted_dim_values(dimension):
     if(d_slice.exists()):
         restrictions = d_slice[0].value.values
         values = {r.attribute.pk: r.value for r in restrictions}
-        values_lists = [[sorted(values[a.pk])] for a in attributes]
+        values_lists = [[values[a.pk]] for a in attributes]
     elif(d_dice.exists()):
         restrictions = d_dice[0].values
         values = {a.pk: [] for a in attributes}
@@ -87,7 +87,7 @@ def get_restricted_dim_values(dimension):
             for ar in dr.values:
                 values[ar.attribute.pk].append(ar.value)
 
-        values_lists = [sorted(values[a.pk]) for a in attributes]
+        values_lists = [values[a.pk] for a in attributes]
     else:
         values_lists = []
         for attribute in attributes:
